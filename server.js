302e5fec16f9f35d48f5c97532a16223d546f1af
RayenNasri <rayennasr679@gmail.com>
2025-03-28 12:13:17 +0100

    jh

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://noble-nurturing.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
